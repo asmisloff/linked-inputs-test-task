734937345612c096c2785f0b13d169cfd7780e3b
Alexey Smisloff <a.smisloff@gmail.com>
2022-03-25 21:59:37 +0300

    reference

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
+import { FC, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const [m, setM] = useState<number | undefined>(undefined)
+  const [color, setColor] = useState("white")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SourceInput
+        value={x}
+        onChange={v => {
+          setX(v)
+        }}
+      />
+      <SourceInput
+        value={y}
+        onChange={v => {
+          setY(v)
+        }}
+      />
+      <ResultInput
+        value={m == undefined ? x + y : m}
+        onChange={v => {
+          setM(v)
+          setColor("yellow")
+        }}
+        onReset={() => {
+          setM(undefined)
+          setColor("white")
+        }}
+        color={color}
+      />
     </div>
   );
+}
+
+const SourceInput: FC<{ value: number, onChange: (value: number) => void }> = ({ value, onChange }) => {
+  return (
+    <input
+      type="number"
+      value={value}
+      onChange={e => onChange(+e.target.value)}
+    />
+  )
+}
+
+const ResultInput: FC<{
+  value: number,
+  onChange: (value: number) => void
+  onReset: () => void,
+  color: string
+}> = ({ value, onChange, onReset, color }) => {
+  return (
+    <input
+      type="number"
+      value={value}
+      onChange={e => onChange(+e.target.value)}
+      onDoubleClick={onReset}
+      style={{ backgroundColor: color }}
+    />
+  )
 }
 
 export default App;
